f8acb8378e11819cf06c2d063a510e50a509cdd7
Alberto Calmini <albycalmini99@gmail.com>
2023-11-02 18:06:07 +0100

    primo commit - interfaccia di base

---
+++ app.py
@@ -0,0 +1,98 @@
+from flask import Flask, render_template, redirect, jsonify, request, url_for, flash, send_from_directory, send_file
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
+from flask_sqlalchemy import SQLAlchemy
+from secrets import token_hex
+import pandas as pd
+import datetime
+import json
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] =  "sqlite:///gv_db.db"
+app.config["SECRET_KEY"] = token_hex(16)
+db = SQLAlchemy(app)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(128), nullable=False)
+
+class IscrizioniEG(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    form_id = db.Column(db.Integer, nullable=False)
+    nome = db.Column(db.String(128), nullable=False)
+    mail = db.Column(db.String(128), nullable=False)
+    zona = db.Column(db.String(128), nullable=False)
+    gruppo = db.Column(db.String(128), nullable=False)
+    specialita = db.Column(db.String(128), nullable=False)
+    tipo = db.Column(db.String(128), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route("/")
+@login_required
+def index():
+    return render_template("index.html")
+
+@app.route("/iscrizioni")
+@login_required
+def iscrizioni():
+    return render_template("iscrizioni.html")
+
+@app.route("/upload_iscrizioni", methods=["POST"])
+@login_required
+def upload_iscrizioni():
+    soci = pd.read_excel(request.files["file"])
+    print(soci)
+    flash("File caricato con successo!", "success")
+    return redirect(url_for("iscrizioni"))
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        utente = User.query.filter_by(username=request.form["username"]).first()
+        if utente:
+            if check_password_hash(utente.password, request.form["passwd"]):
+                login_user(utente)
+                return redirect(url_for("index"))
+            else:
+                flash("Username o Password errati!", "warning")
+        else:
+            flash("Utente inesistente!", "warning")
+    return render_template("login.html")
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+@app.route("/admin", methods=["GET", "POST"])
+@login_required
+def admin():
+    if request.method == "POST":
+        if request.form["id_form"] == "nuovo_utente":
+            utente = User.query.filter_by(username=request.form["username"]).first()
+            if utente is None:
+                password = generate_password_hash(request.form["passwd"])
+                utente = User(username=request.form["username"], password=password)
+                db.session.add(utente)
+                db.session.commit()
+                flash("Utente inserito con successo!", "success")
+            else:
+                flash("Esiste già l'utente "+request.form["username!"], "warning")
+    return render_template("admin.html", utenti=User.query.all())
+
+if __name__ == "__main__":
+    app.run(port=8000, host="0.0.0.0")
