63cc0330f68e5d9ca94c76272c5c71b3f18b1a65
Alberto Calmini <albycalmini99@gmail.com>
2023-12-06 11:12:58 +0100

    aggiunta welcome page

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Gestionale utilizzato per validare form iscrizioni tramite Google Form.
 
 La validazione iscrizioni genera una richiesta alle API di [guidonciniverdi.it](https://guidonciniverdi.it/) che si occupa di creare l'account utente e le pagine del Diario di Bordo
 
+Documentazione per API Google: [https://developers.google.com/forms/api/guides/retrieve-forms-responses?hl=it](https://developers.google.com/forms/api/guides/retrieve-forms-responses?hl=it)
+
+
--- app.py
+++ app.py
@@ -3,11 +3,22 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, log
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 from flask_sqlalchemy import SQLAlchemy
+from apiclient import discovery
+from httplib2 import Http
+from oauth2client import client, file, tools
+import pprint
+import pickle
 import pandas as pd
 import datetime
+import requests
 import secrets
+import base64
 import json
-import os
+
+pp = pprint.PrettyPrinter(indent=4)
+
+with open("credenziali.json", "r") as f:
+    cr = json.load(f)
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] =  "sqlite:///gv_db.db"
@@ -16,7 +27,7 @@ db = SQLAlchemy(app)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=False, unique=True)
+    username = db.Column(db.String(128), nullable=False, unique=True)
     password = db.Column(db.String(128), nullable=False)
 
 class IscrizioniEG(db.Model):
@@ -27,7 +38,23 @@ class IscrizioniEG(db.Model):
     zona = db.Column(db.String(128), nullable=False)
     gruppo = db.Column(db.String(128), nullable=False)
     specialita = db.Column(db.String(128), nullable=False)
+    # tipo indica se conquista o conferma => True se conferma
+    tipo = db.Column(db.Boolean, nullable=False)
+
+class WordpressUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    iscrizioni_id = db.Column(db.Integer, nullable=False)
+    wordpress_id = db.Column(db.Integer, nullable=False)
+    username = db.Column(db.String(128), nullable=False)
+    meta = db.Column(db.JSON, nullable=False)
+
+class WordpressPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    iscrizioni_id = db.Column(db.Integer, nullable=False)
+    wordpress_id = db.Column(db.Integer, nullable=False)
+    # campo di testo per indicare se "presentazione", "prima_impresa", "seconda_impresa", "missione", "extra"
     tipo = db.Column(db.String(128), nullable=False)
+    meta = db.Column(db.JSON, nullable=False)
 
 with app.app_context():
     db.create_all()
@@ -48,6 +75,25 @@ def index():
 @app.route("/iscrizioni")
 @login_required
 def iscrizioni():
+    store = file.Storage("token.json")
+    creds = None
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets("credentials.json", cr["form"]["scopes"])
+        creds = tools.run_flow(flow, store)
+
+    service = discovery.build(
+        "forms",
+        "v1",
+        http=creds.authorize(Http()),
+        discoveryServiceUrl=cr["form"]["discovery_doc"],
+        static_discovery=False,
+    )
+
+    # Prints the responses of your specified form:
+    #form_id = "1sWrdk6uWvrEKqo0ozD3TJQXi2XDTjdNDAEgCjgE0WyM"
+    # result = service.forms().responses().list(formId=form_id).execute()
+    result = service.forms().responses().list(formId=cr["form"]["form_id"]).execute()
+    pp.pprint(result)
     return render_template("iscrizioni.html")
 
 @app.route("/upload_iscrizioni", methods=["POST"])
@@ -60,6 +106,8 @@ def upload_iscrizioni():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if len(User.query.all()) == 0:
+        return render_template("welcome.html")
     if request.method == "POST":
         utente = User.query.filter_by(username=request.form["username"]).first()
         if utente:
@@ -93,6 +141,20 @@ def admin():
             else:
                 flash("Esiste già l'utente "+request.form["username!"], "warning")
     return render_template("admin.html", utenti=User.query.all())
+
+@app.route("/welcome", methods=["POST"])
+def welcome():
+    if len(User.query.all()) > 0:
+        return redirect(url_for("login"))
+    if request.form["passwd"] == request.form["conferma_passwd"]:
+        password = generate_password_hash(request.form["passwd"])
+        utente = User(username=request.form["username"], password=password)
+        db.session.add(utente)
+        db.session.commit()
+        flash("Utente creato con successo!", "success")
+    else:
+        flash("Le password non coincidono!", "warning")
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(port=8000, host="0.0.0.0")
