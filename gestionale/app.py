9f21f4a25c42b2eb902b5e7b9068c3c37b1c6c3d
Alberto Calmini <albycalmini99@gmail.com>
2026-03-15 11:10:00 +0100

    rimosso bug sqc to19

--- gestionale/app.py
+++ gestionale/app.py
@@ -574,11 +574,7 @@ def abilita(id_iscrizione):
             flash("Qualcosa è andato storto con la creazione dell'utente", "warning")
             return redirect(url_for("iscrizioni"))
 
-        tmp_iscrizione.stato = "abilitato"
-        db.session.commit()
-
         tmp_ok = True
-
         tmp_content = requests.get(cr["wordpress"]["url"]+"/posts/8183?context=edit", headers=header).json()["content"]["raw"]
 
         dati = {
@@ -601,6 +597,10 @@ def abilita(id_iscrizione):
                 manda_telegram(User.query.filter_by(username="admin").first().telegram_id, "Problema tecnico!!", testo_telegram)
             except:
                 print("Errore")
+            return redirect(url_for("iscrizioni"))
+
+        tmp_iscrizione.stato = "abilitato"
+        db.session.commit()
 
         testo_mail_sq = f"Congratulazioni {tmp_iscrizione.nome},<br>ecco le credenziali per il Diario di Bordo Digitale, potete accedere <a href=\"https://guidonciniverdi.it/wp-login.php\" target=\"_blank\">cliccando qui</a> oppure scaricando la app.<br><a href=\"https://play.google.com/store/apps/details?id=org.wordpress.android\" target=\"_blank\">Clicca qui per scaricare la app per Android</a><br><a href=\"https://apps.apple.com/it/app/wordpress-website-builder/id335703880\" target=\"_blank\">Clicca qui per scaricare la app per iOS</a><br>Trovate maggiori info qui: <a href=\"https://guidonciniverdi.it/come-funziona/\" target=\"_blank\">guidonciniverdi.it/come-funziona/</a><hr><h4><strong>Credenziali</strong></h4>Username: {tmp_username}<br>Password: {tmp_passwd}"
         manda_mail([tmp_iscrizione.mail], [tmp_iscrizione.mail_capo1, tmp_iscrizione.mail_capo2], "Credenziali Diario di Bordo!", testo_mail_sq, tmp_iscrizione.regione)
